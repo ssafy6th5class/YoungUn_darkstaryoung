f2c707861ee0a21f5af8faafca0b77a7dba46bab
YoungUn Choi <darkstaryoung0406@gmail.com>
2021-12-06 19:15:20 +0900

    12060715

--- [BOJ]_2800_괄호제거/[BOJ]_2800_괄호제거.py
+++ [BOJ]_2800_괄호제거/[BOJ]_2800_괄호제거.py
@@ -5,41 +5,43 @@ string = sys.stdin.readline().rstrip()
 stack = []
 idx_list = []
 
-for st_idx in range(len(string)):
+#부분 1
+# for st_idx in range(len(string)):
     
-    try:
-        if string[st_idx] == '(':
-            stack.append(st_idx)
-        elif string[st_idx] == ')':
-            idx_list.append([stack.pop(), st_idx])
-    except:
-        continue
-
+#     try:
+#         if string[st_idx] == '(':
+#             stack.append(st_idx)
+#         elif string[st_idx] == ')':
+#             idx_list.append([stack.pop(), st_idx])
+#     except:
+#         continue
+# print(idx_list)
+idx_list = [1,2,3,4]
 subset = [[]]
 #부분합 만들기
 for num in idx_list:
     size  = len(subset)
     for y in range(size):
         subset.append(subset[y] + [num])
+print(subset)
+# li = set()
+# result = []
+# list_string = []
+# #입력을 리스트로변경
+# for x in string:
+#     list_string.append(x)
 
-li = set()
-result = []
-list_string = []
-#입력을 리스트로변경
-for x in string:
-    list_string.append(x)
+# #리스트의 인덱스 위치값 없애기
+# for delete in subset[1:]:
+#     tmp = list_string[:]
+#     for a, b in delete:
+#         tmp[a] = ''
+#         tmp[b] = ''
+#     li.add(''.join(tmp))
 
-#리스트의 인덱스 위치값 없애기
-for delete in subset[1:]:
-    tmp = list_string[:]
-    for a, b in delete:
-        tmp[a] = ''
-        tmp[b] = ''
-    li.add(''.join(tmp))
+# for x in li:
+#     result.append(x)
 
-for x in li:
-    result.append(x)
-
-result.sort()
-for x in result:
-    print(x)
+# result.sort()
+# for x in result:
+#     print(x)
